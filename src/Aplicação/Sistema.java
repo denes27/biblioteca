ee8a7febeb6f71d4e78935685d4233d4203f6cf0
N-Santana <barbosa.nataliasantana@gmail.com>
2018-08-05 01:40:09 -0300

    Inicialização da classe Sistema, e aprimoramento da classe Item

---
+++ src/Aplicação/Sistema.java
@@ -0,0 +1,78 @@
+package Aplicação;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Sistema {
+
+		public void Inicializacao(){
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println("Bem-vindo(a) ao sistema da Biblioteca!");
+			System.out.println("Você deseja: 1-Consultar, 2-Reservar, 3-Devolver?");
+			//System.out.println("Fazer login como 'Usuário'(1) ou 'Funcionário'(2)?");
+			int opcao= sc.nextInt();
+			sc.close();
+			filtarOpcao(opcao);
+		}
+		
+		public void filtarOpcao(int opcao) {
+			
+			Item item = new Item();
+			
+			switch(opcao) {
+			case 1:
+				Item.Consultar();
+				break;
+			case 2:
+				try {
+				Item.Reservar(item);
+				} catch (Exception e){
+					Scanner sc = new Scanner(System.in);
+					System.out.println("Reserva mal sucedida!");
+					System.out.println("Deseja: 1-Consultar, 2-Reservar, 3-Devolver outro livro, ou 4-Sair");
+					int opcao2= sc.nextInt();
+					sc.close();
+					filtarOpcao(opcao2);
+				}
+				break;
+			case 3:
+				Item.Devolver();
+				break;
+			case 4:
+				System.out.println("Agradecemos a preferência! Tenha um bom dia!");
+				return;
+			default: 
+				System.out.println("Opção Inválida!");
+				break;
+			}
+		}
+
+		private void opcoesFuncionário() { }
+
+		private void opcoesUsuario() {
+			System.out.println("Usuário já possui cadastro? S/N");
+			Scanner sc = new Scanner(System.in);
+			String resposta= sc.nextLine();
+			if(resposta=="S"){
+				Usuario usuario = new Usuario();
+				//try{
+				System.out.println("Insira Usuário:");
+				String login=sc.nextLine();
+				System.out.println("Insira a Senha:");
+				String senha=sc.nextLine();
+				//Usuario.validacao(login, senha);
+				//} catch(IOException e) {
+					//System.out.println("Usuário inexistente ou inválido!");
+					//opcoesUsuario();
+				//}
+				
+			}
+			if (resposta=="N") {
+					System.out.println("É necessário cadastrar-se para realizar Reservas.");
+					//Usuario.casdastrar();					
+			}
+			sc.close();
+			
+		}
+}
